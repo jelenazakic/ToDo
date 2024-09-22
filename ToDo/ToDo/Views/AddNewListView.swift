d5b34a2b92440c4ec62f292e753d8d2c888e6eca
jelenazakic <jelenazakic111@gmail.com>
2024-09-22 19:28:40 +0200

    New Feature
    Add a button to the navigation bar that opens a sheet for creating a new list.

--- ToDo/ToDo/Views/AddNewListView.swift
+++ ToDo/ToDo/Views/AddNewListView.swift
@@ -1,32 +1,22 @@
-//
-//  AddNewListView.swift
-//  ToDo
-//
-//  Created by Jelena Zakic on 17.9.24..
-//
-
 import SwiftUI
 
 struct AddNewListView: View {
     
-    //  MARK: - Properties
-    
-    @State var newNameList: String = ""
-    @State var lists: [ListModel] = []
-    @Binding var showView: Bool // TODO: ovo nije potrebno
-    // TODO: već postoji built-in properti (proguglaj)
-    
-    //  MARK: - Lifecycle
-    
+    // MARK: - Properties
+    @Binding var newNameList: String
+    @Binding var lists: [ListModel]
+    @Binding var isPresented: Bool 
+
+    // MARK: - Lifecycle
     var body: some View {
         VStack {
             newListTextField
             saveNewListButton
         }
+        .padding()
     }
     
-    //  MARK: - Views
-    
+    // MARK: - Views
     private var newListTextField: some View {
         TextField("Enter New List", text: $newNameList)
             .textFieldStyle(RoundedBorderTextFieldStyle())
@@ -34,39 +24,31 @@ struct AddNewListView: View {
     }
     
     private var saveNewListButton: some View {
-        Button {
-            onSaveNewListButtonTap()
-        } label: {
+        Button(action: onSaveNewListButtonTap) {
             Text("Save")
+                .frame(width: 100, height: 20)
+                .padding(5)
+                .font(.system(size: 15, weight: .bold))
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(20)
         }
-        .frame(width: 100, height: 20)
-        .padding(5)
-        .font(.system(size: 15,weight: .bold, design: .default))
-        .cornerRadius(20)
     }
     
-    //  MARK: - Utility
-    
+    // MARK: - Utility
     private func onSaveNewListButtonTap() {
-        let newList = ListModel(name: newNameList, tasks: [] )
+       
+    
+        let newList = ListModel(name: newNameList, tasks: [])
         lists.append(newList)
-        newNameList.removeAll()
-        
-        showView = false // TODO: ovde pozvati built-in property umesto nas
+        newNameList = ""
+        isPresented = false
     }
 }
+
 #Preview {
-    AddNewListView(
-        lists: [
-            ListModel(
-                name: "Sample List",
-                tasks: [
-                    ItemModel(title: "Sample Task 1", isCompleted: false),
-                    ItemModel(title: "Sample Task 2", isCompleted: true)
-                ]
-            )
-        ],
-        showView: .constant(false)
-    )
+    AddNewListView(newNameList: .constant(""),
+                   lists: .constant([]),
+                   isPresented: .constant(false))
 }
 
--- ToDo/ToDo/Views/ToDoHomePage.swift
+++ ToDo/ToDo/Views/ToDoHomePage.swift
@@ -13,10 +13,12 @@ struct ToDoHomePage: View {
     //  MARK: - Properties
     
     @State private var searchTerm = ""
-    @State private var showCreateNewListSheet: Bool = false
-    @State var checkListCountValue: Int = 0
+    @State var isPresented: Bool = false
+    @State private var newList = ListModel(name: "", tasks: [])
+    @State var items: [ItemModel] = []
+    @State var newNameList: String = " "
     
-    var lists: [ListModel] = [
+    @State var lists: [ListModel] = [
         ListModel(name: "Grocery Shopping",
                   tasks:[
                     ItemModel(title: "Buy milk", isCompleted: false),
@@ -26,7 +28,7 @@ struct ToDoHomePage: View {
                  ),
         ListModel(name: "Daily Task List",
                   tasks:[
-                    ItemModel(title: "Check email", isCompleted: false),
+                    ItemModel(title: "Check email", isCompleted: true),
                     ItemModel(title: "Attend meeting", isCompleted: true),
                     ItemModel(title: "Review project", isCompleted: false)
                   ]
@@ -55,79 +57,67 @@ struct ToDoHomePage: View {
             $0.name.localizedCaseInsensitiveContains(searchTerm)
         }
     }
+    var filteredLists: [ListModel] {
+           guard !searchTerm.isEmpty else { return lists }
+           return lists.filter {
+               $0.name.localizedCaseInsensitiveContains(searchTerm)
+           }
+       }
     
     //  MARK: - Properties
     
     var body: some View {
-        NavigationStack{
-            List(filterList) { list in
-                NavigationLink {
-                    MainListView(
-                        items: list.tasks,
-                        navigationTitle: list.name
-                    )
+            NavigationStack {
+                List(filteredLists) { list in
+                    NavigationLink(destination: MainListView(items: list.tasks, navigationTitle: list.name)) {
+                        listRow(for: list)
+                    }
                 }
-                label: {
-                    Image(systemName: "checklist")
-                    Text(list.name)
-                        .fontWeight(.semibold)
-                        .lineLimit(2)
-                        .minimumScaleFactor(0.5)
-                    
-                    Text(String(checkedCount(items: list.tasks)))
-                        .frame(maxWidth: .infinity,
-                               alignment: .trailing)
-                        .font(.callout)
-                        .fontWeight(.light)
-                    
+                .sheet(isPresented: $isPresented) {
+                    AddNewListView(newNameList: $newNameList, lists: $lists, isPresented: $isPresented)
                 }
-                .padding(.vertical)
+                .navigationBarItems(trailing: addButton)
+                .listRowBackground(Color.blushPlum)
+                .listStyle(PlainListStyle())
+                .navigationTitle("My Lists")
+                .searchable(text: $searchTerm, prompt: "Search List")
             }
-            .sheet(
-                isPresented: $showCreateNewListSheet,
-                content: {
-                    AddNewListView(
-                        lists: [
-                            ListModel(
-                                name: "New name",
-                                tasks: [
-                                    ItemModel(title: "Buy milk", isCompleted: false),
-                                    ItemModel(title: "Get vegetables", isCompleted: false),
-                                    ItemModel(title: "Pick up bread", isCompleted: true)
-                                ]
-                            )
-                        ],
-                        showView: $showCreateNewListSheet
-                    )
-                })
-            .navigationBarItems(
-                trailing:
-                    Button(
-                        " ",
-                        systemImage: "plus",
-                        action: {
-                            showCreateNewListSheet = true
-                        }
-                    )
-                    .font(.system(size: 15,weight: .bold, design: .default))
-            )
-            .listRowBackground(Color.blushPlum)
-            .listStyle(PlainListStyle())
-            .padding(.bottom)
-            .navigationTitle("My Lists")
-            .searchable(text: $searchTerm, prompt: "Search List")
+        }
+        
+        private func listRow(for list: ListModel) -> some View {
+            HStack {
+                Image(systemName: "checklist")
+                Text(list.name)
+                    .fontWeight(.semibold)
+                    .lineLimit(2)
+                    .minimumScaleFactor(0.5)
+                Text("\(checkedCount(items: list.tasks)) / \(countAllTask(items: list.tasks))")
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .font(.callout)
+                    .fontWeight(.light)
+            }
+            .padding(.vertical)
+        }
+        
+        private var addButton: some View {
+            Button(action: {
+                isPresented = true
+            }) {
+                Image(systemName: "plus")
+                    .font(.system(size: 15, weight: .bold))
+            }
+        }
+        
+        func checkedCount(items: [ItemModel]) -> Int {
+            return items.filter { $0.isCompleted }.count
+        }
+        
+        func countAllTask(items: [ItemModel]) -> Int {
+            return items.count
         }
     }
-    
-    func checkedCount(items: [ItemModel]) -> Int {
-        return items.filter { !$0.isCompleted }.count
+
+    #Preview {
+        ToDoHomePage()
     }
     
-}
-
-
-//Vasilijev prvi komit
-
-#Preview {
-    ToDoHomePage(checkListCountValue: 0)
-}
