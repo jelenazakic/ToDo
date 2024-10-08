a9b86b334f57b182505e2f5d0a6e7b5c0b7b2730
jelenazakic <jelenazakic111@gmail.com>
2024-10-08 23:24:09 +0200

    New button animations

--- ToDo/ToDo/Views/AddNewListView.swift
+++ ToDo/ToDo/Views/AddNewListView.swift
@@ -36,6 +36,9 @@ struct AddNewListView: View {
                 .foregroundColor(.white)
                 .background(Color.blue)
                 .cornerRadius(20)
+                .buttonStyle(GrowingButton())
+                
+                
         }
     }
     
@@ -48,8 +51,20 @@ struct AddNewListView: View {
         newNameList = ""
         isPresented = false
     }
+    
+   
 }
-
+struct GrowingButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(.blue)
+            .foregroundStyle(.white)
+            .clipShape(Capsule())
+            .scaleEffect(configuration.isPressed ? 1.2 : 1)
+            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+    }
+}
 #Preview {
     AddNewListView(newNameList: .constant(""),
                    lists: .constant([]),
